a6b5f2e57fe3e76e634f262a3f07ce47d0d0b259
giangtheshy <72980294+giangtheshy@users.noreply.github.com>
2021-05-07 19:06:23 +0700

    update

--- src/components/Home/LeftContent/LeftContent.jsx
+++ src/components/Home/LeftContent/LeftContent.jsx
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import stringSimilarity from "string-similarity";
 import "./LeftContent.scss";
 import ListFilm from "../../utils/ListFilm/ListFilm";
+import compare from "components/utils/similarString";
 const LeftContent = ({ films }) => {
   const recommenderFilm = useCallback(() => {
     const title = localStorage.getItem("title");
@@ -9,10 +10,11 @@ const LeftContent = ({ films }) => {
       return [];
     }
     const mapToArrayRecommendation = films.map((film) => {
-      const ratingTitle = stringSimilarity.compareTwoStrings(film.title, title);
+      const ratingTitle = compare(film.title, title);
+      // const ratingTitle = stringSimilarity.compareTwoStrings(film.title, title);
       return { ...film, rating: ratingTitle };
     });
-    return mapToArrayRecommendation.sort((a, b) => a.rating - b.rating).slice(1, 6);
+    return mapToArrayRecommendation.sort((a, b) => b.rating - a.rating).slice(1, 6);
   }, [films?.length]);
   if (!films) return <></>;
   return (
