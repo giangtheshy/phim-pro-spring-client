4de216bbd8bd923cbbdbf7fbbc7e5b217f3c103d
giangtheshy <72980294+giangtheshy@users.noreply.github.com>
2021-05-14 22:14:02 +0700

    add share fb

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import MetaTags from "react-meta-tags";
 import "./Home.scss";
 import RightContent from "../../components/Home/RightContent/RightContent";
 import LeftContent from "../../components/Home/LeftContent/LeftContent";
@@ -10,6 +11,15 @@ const Home = () => {
   const films = useSelector((state) => state.films.films);
   return (
     <>
+      <MetaTags>
+        <title>Phim hay mới ra mắt | Xem những bộ phim hấp dẫn trên phim-pro</title>
+        <meta
+          name="og:description"
+          content="Phim pro là trang web tuyển chọn những bộ phim hay và mới nhất với chất lượng tốt nhất đưa đến cho người xem . Click để chọn bộ phim yêu thích...."
+        />
+        <meta property="og:title" content="Phim hay mới ra mắt | Xem những bộ phim hấp dẫn trên phim-pro" />
+        <meta property="og:image" content="https://i.ytimg.com/vi/v7JMxXntfXU/maxresdefault.jpg" />
+      </MetaTags>
       <Search setValue={setValue} value={value} />
       <div className="home">
         <RightContent value={value} setValue={setValue} films={films} />
